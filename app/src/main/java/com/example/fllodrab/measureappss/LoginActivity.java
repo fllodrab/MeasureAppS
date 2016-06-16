3fd66709b470ba4fa144f6508851db54be5b3383
fllodrab <llodrab@gmail.com>
2016-06-16 21:18:00 +0200

    Esto cierra el issue #1, closes #1

--- app/src/main/java/com/example/fllodrab/measureappss/LoginActivity.java
+++ app/src/main/java/com/example/fllodrab/measureappss/LoginActivity.java
@@ -15,7 +15,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import butterknife.ButterKnife;
 import butterknife.Bind;
 
